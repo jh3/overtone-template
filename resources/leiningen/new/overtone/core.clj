a3de18246ba3474418e37cf305bc19307620b540
Joe Hassick <joe.hassick@gmail.com>
2016-05-22 12:34:54 -0400

    Fix namespace definition

--- resources/leiningen/new/overtone/core.clj
+++ resources/leiningen/new/overtone/core.clj
@@ -3,13 +3,10 @@
   (:require [overtone.studio.fx]
             [overtone.inst.synth]))
 
-(definst lead [pitch 440 attack 0.01 sustain 0.4 release 0.05 vol 0.4] 
-  (lpf (* (env-gen (lin attack sustain release) 1 1 0 1 FREE) 
-          (saw pitch) 
-          vol) 
+(definst lead [pitch 440 attack 0.01 sustain 0.4 release 0.05 vol 0.4]
+  (lpf (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+          (saw pitch)
+          vol)
        5000))
 
-(defn -main [& args]
-  (println "{{name}} is now playing...")
-  (println "Press Ctrl+C to stop the performance.")
-  (lead 220))
+(lead 220)
--- src/leiningen/new/overtone.clj
+++ src/leiningen/new/overtone.clj
@@ -5,17 +5,18 @@
 (def render (renderer "overtone"))
 
 (defn overtone
-  "overtone project template"
+  "Generate a new Leiningen project with Overtone as a dependency."
   [name]
   (let [render (renderer "overtone")
         data {:name name
-              :sanitized (name-to-path name)}]
+              :sanitized (name-to-path name)
+              :namespace (str name ".core")
+              :year "2016"}]
     (main/info "Generating fresh 'lein new' overtone project.")
     (->files data
              ["project.clj" (render "project.clj" data)]
              ["README.md" (render "README.md" data)]
              ["doc/intro.md" (render "intro.md" data)]
              [".gitignore" (render ".gitignore" data)]
-             ["src/{{sanitized}}.clj" (render "core.clj" data)]
-             ["test/{{sanitized}}_test.clj" (render "test.clj" data)]
+             ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
              "resources")))
