d737a3979b94bd376d62a9f7f74061caff6fe3eb
Joe Hassick <joe.hassick@gmail.com>
2016-05-22 11:43:36 -0400

    Update template files

--- resources/leiningen/new/overtone/core.clj
+++ resources/leiningen/new/overtone/core.clj
@@ -1,6 +1,15 @@
-(ns {{name}}.core)
+(ns {{namespace}}
+  (use [overtone.live])
+  (:require [overtone.studio.fx]
+            [overtone.inst.synth]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, Overtone World!"))
+(definst lead [pitch 440 attack 0.01 sustain 0.4 release 0.05 vol 0.4] 
+  (lpf (* (env-gen (lin attack sustain release) 1 1 0 1 FREE) 
+          (saw pitch) 
+          vol) 
+       5000))
+
+(defn -main [& args]
+  (println "{{name}} is now playing...")
+  (println "Press Ctrl+C to stop the performance.")
+  (lead 220))
